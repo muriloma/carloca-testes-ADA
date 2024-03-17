8322fe2b6439d68d5b4b941bb8ba5f104c5015f0
Murilo M Andrade <andrade.murilom@gmail.com>
2024-03-17 19:42:32 -0300

    Ajuste nos testes de integracao do usuario

--- src/controllers/user-ctrl.js
+++ src/controllers/user-ctrl.js
@@ -5,15 +5,15 @@ class UserController {
     static async create(req, res) {
         try {
             const { name, email, password } = req.body
-    
-            if(!Email.isValid(email)) {
-                throw { status: 400, message: 'Email inválido'}
+
+            if (!Email.isValid(email)) {
+                throw { status: 400, message: 'Email inválido' }
             }
-            
-            if(!password) {
-                throw { status: 400, message: 'Senha inválida'}
+
+            if (!password) {
+                throw { status: 400, message: 'Senha inválida' }
             }
-    
+
             const { id } = await UserService.createUser({ name, email, password })
 
             return res.status(200).json({ id })
@@ -24,9 +24,9 @@ class UserController {
 
     static async changePassword(req, res) {
         const { userEmail: email } = req
-        
+
         console.log('Alterando senha...')
-        return res.status(200).json({ message: 'ok'})
+        return res.status(200).json({ message: 'ok' })
     }
 }
 
