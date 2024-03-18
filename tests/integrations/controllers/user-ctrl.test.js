61870b17d1359dc004aae781bea2880991b08392
Ailézig Vilar <ailezigvilar@gmail.com>
2024-03-17 22:27:03 -0300

    TESTS: Fixing MongoDB integration for integration tests

--- tests/integrations/controllers/user-ctrl.test.js
+++ tests/integrations/controllers/user-ctrl.test.js
@@ -8,33 +8,30 @@ const User = require('../../../src/schemas/User')
 describe('[Integration] Client cotroller tests', () => {
     beforeAll(async () => {
         mongoose.connect(process.env.MONGO_DB_URL)
-
-        // await User.create({
-        //     name: 'Any User',
-        //     email: 'any@mail.com',
-        //     password: 'anypassword'
-        // })
+        await User.create({
+            name: 'Any User',
+            email: 'any@mail.com',
+            password: 'anypassword'
+        })
     })
 
     afterAll(async () => {
-        // await User.deleteMany({})
-        await mongoose.connection.close()
+        await User.deleteMany({});
+        await mongoose.connection.close();
     })
 
     test('Should create a new user at database', async () => {
-        const resSpy = jest.spyOn(res, 'status')
-        await UserController.create(req.success, res)
+        const resSpy = jest.spyOn(res, 'status');
+        await UserController.create(req.success, res);
 
-        expect(resSpy).toHaveBeenCalledWith(200)
+        expect(resSpy).toHaveBeenCalledWith(200);
     })
 
-    test('Should throw status code 400 if an invalid email is invalid', async () => {
+    test('Should throw status code 400 if an invalid email is used', async () => {
         const resSpy = jest.spyOn(res, 'status')
 
         await UserController.create(req['invalid-email'], res)
 
         expect(resSpy).toHaveBeenCalledWith(400)
     })
-
-
 })
